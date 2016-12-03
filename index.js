74980e0680588e9b6fed55356eb2807e9fceaa45
Samir Talwar <samir@noodlesandwich.com>
2016-12-03 18:26:17 +0000

    Make a Watson class.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const watson = require('./watson')
+const Watson = require('./watson')
 const youtube = require('./youtube')
 const fs = require('fs')
 const koa = require('koa')
 const route = require('koa-route')
 
 const config = JSON.parse(fs.readFileSync('config.json'))
+const watson = Watson(config.watson)
 
 const app = koa()
 app.use(route.get('/:videoId', function*(videoId) {
   console.log(`HTTP GET /${videoId}`)
   this.type = 'application/json'
   this.body = yield youtube.getYouTubeAudio(videoId)
-    .then(watson.watsonSpeechToText.bind(this, config.watson, videoId))
+    .then(audioFile => watson.speechToText(videoId, audioFile))
 }))
 
 new Promise(resolve => {
--- watson.js
+++ watson.js
@@ -2,55 +2,57 @@ const {denodeify} = require('./promise')
 const watson = require('watson-developer-cloud')
 const fs = require('fs')
 
-exports.watsonSpeechToText = function (config, videoId, audioFile) {
-  const speechToText = watson.speech_to_text(Object.assign({
-    version: 'v1',
-    url: 'https://stream.watsonplatform.net/speech-to-text/api'
-  }, config))
+module.exports = config => ({
+  speechToText: (videoId, audioFile) => {
+    const speechToText = watson.speech_to_text(Object.assign({
+      version: 'v1',
+      url: 'https://stream.watsonplatform.net/speech-to-text/api'
+    }, config))
 
-  const transcriptFile = `videos/${videoId}.transcript`
-  return denodeify(fs.readFile)(transcriptFile)
-    .catch(() => new Promise(function (resolve, reject) {
-      const params = {
-        content_type: 'audio/flac',
-        timestamps: true,
-        continuous: true
-      }
-
-      const results = []
-
-      // create the stream
-      const recognizeStream = speechToText.createRecognizeStream(params)
-
-      // pipe in some audio
-      fs.createReadStream(audioFile).pipe(recognizeStream)
-
-      // listen for 'data' events for just the final text
-      // listen for 'results' events to get the raw JSON with interim results, timings, etc.
-
-      recognizeStream.setEncoding('utf8') // to get strings instead of Buffers from `data` events
-
-      recognizeStream.on('results', event => {
-        if (event.results[0].final) {
-          results.push(event)
+    const transcriptFile = `videos/${videoId}.transcript`
+    return denodeify(fs.readFile)(transcriptFile)
+      .catch(() => new Promise((resolve, reject) => {
+        const params = {
+          content_type: 'audio/flac',
+          timestamps: true,
+          continuous: true
         }
-      })
 
-      ;['data', 'error', 'end'].forEach(function (eventName) {
-        recognizeStream.on(eventName, console.log.bind(console, eventName + ' event:'))
-      })
+        const results = []
 
-      recognizeStream.on('error', reject)
+        // create the stream
+        const recognizeStream = speechToText.createRecognizeStream(params)
 
-      recognizeStream.on('end', function () {
-        fs.writeFile(transcriptFile, JSON.stringify(results), function (err) {
-          if (err) {
-            reject(err)
-          } else {
-            console.log('Transcription complete.')
-            resolve(results)
+        // pipe in some audio
+        fs.createReadStream(audioFile).pipe(recognizeStream)
+
+        // listen for 'data' events for just the final text
+        // listen for 'results' events to get the raw JSON with interim results, timings, etc.
+
+        recognizeStream.setEncoding('utf8') // to get strings instead of Buffers from `data` events
+
+        recognizeStream.on('results', event => {
+          if (event.results[0].final) {
+            results.push(event)
           }
         })
-      })
-    }))
-}
+
+        ;['data', 'error', 'end'].forEach(eventName => {
+          recognizeStream.on(eventName, console.log.bind(console, eventName + ' event:'))
+        })
+
+        recognizeStream.on('error', reject)
+
+        recognizeStream.on('end', () => {
+          fs.writeFile(transcriptFile, JSON.stringify(results), error => {
+            if (error) {
+              reject(error)
+            } else {
+              console.log('Transcription complete.')
+              resolve(results)
+            }
+          })
+        })
+      }))
+  }
+})
