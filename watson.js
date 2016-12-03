fc15ca140b9e506b112784818bde7ed72b8df149
Samir Talwar <samir@noodlesandwich.com>
2016-12-03 20:53:33 +0000

    Run the whole speech analysis at once.

--- watson.js
+++ watson.js
@@ -5,42 +5,21 @@ const fs = require('fs')
 const ToneAnalyzerVersion = '2016-05-19'
 
 module.exports = config => {
-  const speechToText = (videoId, audioFile, onLine) => new Promise((resolve, reject) => {
+  const speechToText = (videoId, audioFile, onLine) => {
     const speechToText = watson.speech_to_text(Object.assign({
-      version: 'v1',
-      url: 'https://stream.watsonplatform.net/speech-to-text/api'
+      version: 'v1'
     }, config['speech-to-text']))
 
     const params = {
       content_type: 'audio/flac',
+      audio: fs.createReadStream(audioFile),
       timestamps: true,
       continuous: true
     }
 
     // create the stream
-    const recognizeStream = speechToText.createRecognizeStream(params)
-
-    // pipe in some audio
-    fs.createReadStream(audioFile).pipe(recognizeStream)
-
-    // listen for 'data' events for just the final text
-    // listen for 'results' events to get the raw JSON with interim results, timings, etc.
-
-    recognizeStream.setEncoding('utf8') // to get strings instead of Buffers from `data` events
-
-    recognizeStream.on('results', event => {
-      if (event.results[0].final) {
-        onLine(event)
-      }
-    })
-
-    ;['data', 'error', 'end'].forEach(eventName => {
-      recognizeStream.on(eventName, console.log.bind(console, eventName + ' event:'))
-    })
-
-    recognizeStream.on('error', reject)
-    recognizeStream.on('end', resolve)
-  })
+    return denodeify(speechToText.recognize.bind(speechToText))(params)
+  }
 
   const toneAnalyzer = text => {
     const toneAnalyzer = watson.tone_analyzer(Object.assign({
@@ -53,18 +32,22 @@ module.exports = config => {
 
   const analyze = (videoId, audioFile) => {
     const transcriptFile = `videos/${videoId}.transcript`
-    const results = []
     return denodeify(fs.readFile)(transcriptFile)
       .then(JSON.parse)
       .catch(() =>
-        speechToText(videoId, audioFile, event =>
-          toneAnalyzer(event.results[0].alternatives[0].transcript).then(tone => {
-            results.push(Object.assign({}, event, tone))
+        speechToText(videoId, audioFile)
+          .then(transcript =>
+            Promise.all(transcript.results.map(result =>
+              toneAnalyzer(result.alternatives[0].transcript)
+                .then(tone => Object.assign({}, result, tone))))
+              .then(resultsWithTones => {
+                transcript.results = resultsWithTones
+                return transcript
+              }))
+          .then(transcript => denodeify(fs.writeFile)(transcriptFile, JSON.stringify(transcript)))
+          .then(() => {
+            console.log('Transcription complete.')
           }))
-        .then(() => denodeify(fs.writeFile)(transcriptFile, JSON.stringify(results)))
-        .then(() => {
-          console.log('Transcription complete.')
-        }))
   }
 
   return {
