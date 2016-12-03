28d91c1e65c4ba71c04e51cbca2710a9edfa5e3f
Samir Talwar <samir@noodlesandwich.com>
2016-12-03 16:18:36 +0000

    Autoformat.

--- watson.js
+++ watson.js
@@ -1,61 +1,58 @@
-var watson = require('watson-developer-cloud');
-var fs = require('fs');
-var path = require('path');
-var Promise = require('bluebird');
+var watson = require('watson-developer-cloud')
+var fs = require('fs')
+var path = require('path')
+var Promise = require('bluebird')
 
-var speech_to_text = watson.speech_to_text({
+var speechToText = watson.speech_to_text({
   username: <YOUR CREDS HERE>,
   password: <YOUR CREDS HERE>,
   version: 'v1',
-  url: 'https://stream.watsonplatform.net/speech-to-text/api',
-});
+  url: 'https://stream.watsonplatform.net/speech-to-text/api'
+})
 
-exports.watsonSpeechToText = function(audioFile) {
-
-  return new Promise(function(resolve, reject) {
-
+exports.watsonSpeechToText = function (audioFile) {
+  return new Promise(function (resolve, reject) {
     var params = {
       content_type: 'audio/flac',
       timestamps: true,
       continuous: true
-    };
+    }
 
-    var results = [];
+    var results = []
 
     // create the stream
-    var recognizeStream = speech_to_text.createRecognizeStream(params);
+    var recognizeStream = speechToText.createRecognizeStream(params)
 
     // pipe in some audio
-    fs.createReadStream(audioFile).pipe(recognizeStream);
+    fs.createReadStream(audioFile).pipe(recognizeStream)
 
     // listen for 'data' events for just the final text
     // listen for 'results' events to get the raw JSON with interim results, timings, etc.
 
-    recognizeStream.setEncoding('utf8'); // to get strings instead of Buffers from `data` events
+    recognizeStream.setEncoding('utf8') // to get strings instead of Buffers from `data` events
 
-    recognizeStream.on('results', function(e) {
+    recognizeStream.on('results', function (e) {
       if (e.results[0].final) {
-        results.push(e);
+        results.push(e)
       }
     });
 
-    ['data', 'results', 'error', 'connection-close'].forEach(function(eventName) {
-      recognizeStream.on(eventName, console.log.bind(console, eventName + ' event: '));
-    });
+    ['data', 'results', 'error', 'connection-close'].forEach(function (eventName) {
+      recognizeStream.on(eventName, console.log.bind(console, eventName + ' event: '))
+    })
 
-    recognizeStream.on('error', function(err) {
-      util.handleError('Error writing to transcript.json: ' + err);
-    });
+    recognizeStream.on('error', reject)
 
-    recognizeStream.on('connection-close', function() {
-    	var transcriptFile = path.join(__dirname, 'transcript.json');
+    recognizeStream.on('connection-close', function () {
+      var transcriptFile = path.join(__dirname, 'transcript.json')
 
-      fs.writeFile(transcriptFile, JSON.stringify(results), function(err) {
+      fs.writeFile(transcriptFile, JSON.stringify(results), function (err) {
         if (err) {
-          util.handleError(err);
+          reject(err)
+        } else {
+          resolve()
         }
-        resolve();
-      });
-    });
-  });
-};
+      })
+    })
+  })
+}
--- youtube.js
+++ youtube.js
@@ -1,34 +1,34 @@
-var spawn = require('child_process').spawn;
-var Promise = require('bluebird');
-var ffmpeg = require('fluent-ffmpeg');
-var path = require('path');
+var spawn = require('child_process').spawn
+var Promise = require('bluebird')
+var ffmpeg = require('fluent-ffmpeg')
+var path = require('path')
 
-exports.getYouTubeAudio = function(videoId){
-	return new Promise(function(resolve, reject){
-      // Install youtube_dl locally: brew install youtube-dl
-    youtube_dl = spawn('youtube-dl', ['--extract-audio', '--audio-format', 'mp3', '-o', 'file.%(ext)s', "http://www.youtube.com/watch?v=" + videoId]);
+exports.getYouTubeAudio = function (videoId) {
+  return new Promise(function (resolve, reject) {
+      // Install youtube-dl locally: brew install youtube-dl
+    const youtubeDl = spawn('youtube-dl', ['--extract-audio', '--audio-format', 'mp3', '-o', 'file.%(ext)s', 'http://www.youtube.com/watch?v=' + videoId])
 
-    youtube_dl.stdout.on('data', function(data){
-      console.log(data.toString());
-    });
+    youtubeDl.stdout.on('data', function (data) {
+      console.log(data.toString())
+    })
 
-    youtube_dl.stderr.on('data', function(data){
-      process.stderr.write(data);
-    });
+    youtubeDl.stderr.on('data', function (data) {
+      process.stderr.write(data)
+    })
 
     // brew install ffmpeg
-    youtube_dl.on('exit', function(){
-      var mp3File = path.join(__dirname, 'file.mp3');
+    youtubeDl.on('exit', function () {
+      var mp3File = path.join(__dirname, 'file.mp3')
       var flacFile = path.join(__dirname, 'file.flac')
       ffmpeg(mp3File)
         .output(flacFile)
-        .on('end', function(){
-          resolve();
+        .on('end', function () {
+          resolve()
         })
-        .on('error', function(err){
-          reject(err);
+        .on('error', function (err) {
+          reject(err)
         })
-        .run();
-    });
-  });
-};
+        .run()
+    })
+  })
+}
